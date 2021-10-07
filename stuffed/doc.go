c313049bb891f13b1915309f74280fb50e78b429
Douglas Creager <dcreager@dcreager.net>
2021-10-06 21:30:12 -0400

    Move documentation into main package file

--- stuffed/doc.go
+++
@@ -1,5 +0,0 @@
-// Package stuffed provides a Go implementation of Paul Khuong's stuffed records
-// encoding.  This is a modified version of Consistent Overhead Byte Stuffing
-// (COBS), which uses the uncommon two-byte sequence `0xfe 0xfd` as the record
-// delimiter, instead of the more common one-byte sequence `0x00`.
-package stuffed
--- stuffed/stuffed.go
+++ stuffed/stuffed.go
@@ -1,3 +1,7 @@
+// Package stuffed provides a Go implementation of Paul Khuong's stuffed records
+// encoding.  This is a modified version of Consistent Overhead Byte Stuffing
+// (COBS), which uses the uncommon two-byte sequence `0xfe 0xfd` as the record
+// delimiter, instead of the more common one-byte sequence `0x00`.
 package stuffed
 
 import (
